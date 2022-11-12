b2972a66518e29735437ade715943908a69c75dd
GreenSnake1975 <GreenSnake1975@hotmail.com>
2022-11-13 01:09:36 +0700

    First commit

---
+++ FindHostInDHCP.ps1
@@ -0,0 +1,23 @@
+﻿<#
+Поиск компа с указанным именем на всех dhcp-серверах авторизованных в AD , 
+поиск производить на одном из них (DC)
+#>
+param(
+    [String] $HostName,
+    [String] $dc
+)
+
+Invoke-Command -ComputerName $dc -ScriptBlock {
+    param($CompName)
+    Get-DhcpServerInDC | 
+    %{ 
+        $dhcpSrv = $_.DnsName ; 
+        try {
+            Test-Connection -Count 1 -ComputerName $dhcpSrv -ErrorAction stop | Out-Null ;
+            Get-DhcpServerv4Scope -computerName $dhcpSrv | Get-DhcpServerv4Lease -computername $dhcpSrv 
+        } catch {
+        }
+    } | 
+    Where-Object -Property HostName -iLike "$CompName.*" | 
+    Select-Object -Property @{'n'='DHCPserver';'e'={$dhcpSrv}},* 
+} -ArgumentList $HostName 
